c37e4e904ba48ab05b7930d0b524b1ae65e8a1d0
emigu777 <emily.guillou1@gmail.com>
2023-06-24 13:52:16 +0200

    camera fixed
    been trying to get the mushroom script to take the player script for 2 hours, also been trying to get the player to not move through objects; nothing has worked so far and there is no button for physics material ???? but the camera doesn't tilt anymore so that's a win

---
+++ Assets/Scripts/Player_Script.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Script : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _animator = new Animator();
+
+    [SerializeField] 
+    private Rigidbody RB;
+
+    [SerializeField] 
+    private float _speed = 5f;
+    [SerializeField]
+    private float _jumpingSpeed = 10f;
+
+    private int _score = 0; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // START POSITION 
+        transform.position = new Vector3(0f, 0f, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        
+    }
+
+    private void Move()
+    {
+        // jumps with space
+        if (Input.GetKeyDown("space"))
+        {
+            RB.velocity += new Vector3(0f, _jumpingSpeed, 0f);
+        }
+        if (Input.GetKey("up"))
+        {
+            
+            RB.MovePosition(transform.position + new Vector3(0f,0f,5f) * Time.deltaTime * _speed);
+        }
+        if (Input.GetKey("down"))
+        {
+            RB.MovePosition(transform.position + new Vector3(0f,0f,-5f) * Time.deltaTime * _speed);
+        }
+        if (Input.GetKey("left"))
+        {
+            RB.MovePosition(transform.position + new Vector3(-5f,0f,0f) * Time.deltaTime * _speed);
+        }
+        if (Input.GetKey("right"))
+        {
+            RB.MovePosition(transform.position + new Vector3(5f,0f,0f) * Time.deltaTime * _speed);
+        }
+    }
+
+    public void ScoreUpdate()
+    {
+        _score += 1;
+    }
+}
